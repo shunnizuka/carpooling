6e7c4488a169bcd9c08901b585465d14ae260602
x240\Shune <smiley.slyt@gmail.com>
2019-03-09 13:39:27 +0800

    Update databse

--- carpool.sql
+++ carpool.sql
@@ -1,37 +1,82 @@
 create table Users(
-	userID integer notnull,
-	userName varchar(100) notnull,
-	userPhone integer notnull,
+	userID serial,
+	userName varchar(100) not null,
+	userPhone integer not null,
 	userEmail varchar(100),
-	userPassword varchar(100) notnull
-	primary key (userID)
+	userPassword varchar(100) not null,
+	primary key(userID),
+	unique(userName, userPhone, userEmail) 
 );
 
 create table Passengers(
-	userID integer notnull,
-	userName varchar(100) notnull,
-	userPhone integer notnull,
+	userID serial,
+	userName varchar(100) not null,
+	userPhone integer not null,
 	userEmail varchar(100),
-	userPassword varchar(100) notnull
-	rating integer default 0
-	primary key (userID)
+	userPassword varchar(100) not null,
+	rating integer default 0,
+	primary key(userID),
 	foreign key(userID, userName, userPhone, userEmail, userPassword) 
 		references Users(userID, userName, userPhone, userEmail, userPassword)
-)
+);
 
 create table Drivers(
-	userID integer notnull,
-	userName varchar(100) notnull,
-	userPhone integer notnull,
+	userID integer not null,
+	userName varchar(100) not null,
+	userPhone integer not null,
 	userEmail varchar(100),
-	userPassword varchar(100) notnull
-	rating integer default 0
-	primary key (userID)
+	userPassword varchar(100) not null,
+	rating integer default 0,
+	primary key(userID),
 	foreign key(userID, userName, userPhone, userEmail, userPassword) 
 		references Users(userID, userName, userPhone, userEmail, userPassword)
-)
+);
 
 create table Rides(
-	rideID integer notnull,
-	rideTimeStamp ti
-)
+	rideID serial,
+	rideDate date not null,
+	rideTime time not null,
+	rideDestination varchar(100) not null,
+	rideOrigin varchar(100) not null,
+	rideCurrentPrice integer not null,
+	ridePlateNumber varchar(100) not null,
+	primary key(rideID),
+	foreign key(ridePlateNumber) references vehicles(plateNumber),
+	check (ridetime) >= current_time,
+	check (rideDate) >= current_date 
+);
+
+create table Vehicles(
+	plateNumber varchar(100) not null,
+	carType varchar(100) not null,
+	carBrand varchar(100) not null,
+	carModel varchar(100) not null,
+	carColour varchar(100) not null,
+	primary key(plateNumber)
+);
+
+create table Preferences(
+	userID integer not null,
+	smoker boolean,
+	male boolean,
+	highSpeed boolean,
+	foreign key(userID) references Passengers(userID),
+	on delete cascade
+);
+
+create table Payment Method(
+	cardNumber integer not null,
+	cardType varchar(100) not null,
+	cardCVI integer(100) not null,
+	primary key(cardNumber)
+);
+
+INSERT INTO Passengers (userName, userPhone, userPassword)
+VALUES ('Beatrice', 93234567, password ),
+ ('Leslie Cole', 91234567, password );
+
+INSERT INTO Drivers (userName, userPhone, userPassword)
+VALUES ('Leslie Cole', 91234567, password ),
+ ('Bava', 92234567, password );
+
+
--- routes/select.js
+++ routes/select.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool } = require('pg')
-/* --- V7: Using Dot Env ---
+//const { Pool } = require('pg')
+ //--- V7: Using Dot Env ---
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -10,7 +10,7 @@ const pool = new Pool({
   password: '********',
   port: 5432,
 })
-*/
+
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
