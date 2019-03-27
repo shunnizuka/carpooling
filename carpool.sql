4ccb76337f548638432e19fdb00b1cda7867c5b6
bava98 <bavadharini.s11@gmail.com>
2019-03-27 19:37:28 +0800

    updates to schema

--- carpool.sql
+++ carpool.sql
@@ -7,51 +7,46 @@ DROP TABLE if exists Preferences cascade;
 DROP TABLE if exists PaymentMethod cascade;
 
 create table Users(
-	userName varchar(100) not null,
+	userName varchar(20) not null,
 	userPhone integer not null,
-	userPassword varchar(100) not null,
+	userPassword varchar(20) not null,
 	primary key(userName)
 );
 
 create table Passengers(
-	userName varchar(100) not null,
-	userPhone integer not null,
-	userPassword varchar(100) not null,
+	userName varchar(20) not null,
 	rating integer default 0,
 	primary key(userName),
 	foreign key(userName) references Users(userName)
 );
 
 create table Vehicles(
-	plateNumber varchar(100) not null,
-	carType varchar(100) not null,
-	carBrand varchar(100) not null,
-	carModel varchar(100) not null,
-	carColour varchar(100) not null,
+	plateNumber varchar(8) not null,
+	carType varchar(20) not null,
+	carBrand varchar(20) not null,
+	carModel varchar(20) not null,
+	carColour varchar(20) not null,
 	primary key(plateNumber)
 );
 
 create table Drivers(
-	userName varchar(100) not null,
-	userPhone integer not null,
-	userPassword varchar(100) not null,
+	userName varchar(20) not null,
 	rating integer,
-	plateNumber varchar(100) not null,
-	primary key(userName),
+	plateNumber varchar(20) not null,
+	primary key(userName, plateNumber),
 	foreign key(userName) references Users(userName),
 	foreign key(plateNumber) references Vehicles(plateNumber)
 );
 
 create table Rides(
-	rideID serial,
 	rideDate date not null,
 	rideTime time not null,
-	rideDestination varchar(100) not null,
-	rideOrigin varchar(100) not null,
+	rideDestination varchar(20) not null,
+	rideOrigin varchar(20) not null,
 	rideCurrentPrice float not null,
-	ridePlateNumber varchar(100) not null,
-	rideDriverName varchar(100) not null,
-	primary key(rideID),
+	ridePlateNumber varchar(8) not null,
+	rideDriverName varchar(20) not null,
+	primary key(ridePlateNumber),
 	foreign key(ridePlateNumber) references Vehicles(plateNumber),
 	foreign key(rideDriverName) references Drivers(userName),
 	check ((ridetime) >= current_time),
@@ -59,26 +54,26 @@ create table Rides(
 );
 
 create table Bids(
-	bidderName varchar(100) not null,
-	rideID integer not null,
+	bidderName varchar(20) not null,
+	bidsPlateNumber varchar(8),
 	price float not null,
+	primary key(bidderName, bidsPlateNumber, price),
 	foreign key(bidderName) references Passengers(userName),
-	foreign key(rideID) references Rides(rideID),
+	foreign key(bidsPlateNumber) references Rides(ridePlateNumber),
 	check (price > 0)
 );
 
 create table Preferences(
-	userName varchar(100) not null,
-	smoker boolean,
-	male boolean,
-	highSpeed boolean,
+	userName varchar(20) not null,
+	preferenceId varchar(20),
+	primary key(preferenceId, userName),
 	foreign key(userName) references Passengers(userName)
 	on delete cascade
 );
 
 create table PaymentMethod(
 	cardNumber integer not null,
-	cardType varchar(100) not null,
+	cardType varchar(20) not null,
 	cardCVI integer not null,
 	primary key(cardNumber)
 );
