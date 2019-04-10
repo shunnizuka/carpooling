0f430d57e708aadb88c1730cff47ef92f2f9eaef
x240\Shune <smiley.slyt@gmail.com>
2019-04-10 17:38:32 +0800

    Signup driver working

--- routes/signup_driver.js
+++ routes/signup_driver.js
@@ -26,14 +26,39 @@ router.post('/', function(req, res, next) {
 	var username  = req.body.username;
 	var phone    = req.body.phone;
 	var password = req.body.password;
+	var platenumber = req.body.platenumber;
+	var carbrand = req.body.carbrand;
+	var carcolor = req.body.carcolor;
+	var carmodel = req.body.carmodel;
+	var cartype = req.body.cartype;
 	
 	// Construct Specific SQL Query
 	var insert_query_users = 'INSERT INTO users VALUES' + "('" + username + "','" + phone + "','" + password + "')";
 	var insert_query_drivers = 'INSERT INTO drivers VALUES' + "('" + username + "')";
+	var insert_query_vehicles = 'INSERT INTO vehicles VALUES' + "('" + platenumber + "','" + username + "','" 
+	+ cartype + "','" + carbrand + "','" + carmodel + "','" + carmodel + "')";
 
-	pool.query(insert_query, (err, data) => {
-		res.redirect('/login');
-	});
+	console.log(insert_query_drivers);
+	console.log(insert_query_users);
+	console.log(insert_query_vehicles);
+	(async () => {
+	
+		const client = await pool.connect()
+		//transaction to add users and passenger
+		try {
+			await client.query('BEGIN');
+			const { rows } = await client.query(insert_query_users);
+			await client.query(insert_query_drivers);
+			await client.query(insert_query_vehicles);
+			await client.query('COMMIT');
+			res.redirect('/login');
+		} catch (e) {
+			await client.query('ROLLBACK')
+			throw e
+		} finally {
+			client.release()
+		}
+	})().catch(e => console.error(e.stack))
 });
 
 module.exports = router;
